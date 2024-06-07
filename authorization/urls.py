fc5ffbcd37d4c16d3d8267dff2e79b9da1faffdc
Vadim136 <barkal.bk@yandex.ru>
2024-06-07 21:48:34 +0300

    Add login view

--- authorization/urls.py
+++ authorization/urls.py
@@ -4,9 +4,12 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from authorization.views import LoginAPIView
+
 
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', LoginAPIView.as_view(), name='login'),
 
 ]
