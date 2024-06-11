184f306a263be52d120c2e440b4ff246e2b5f835
Shish-ai-ai <ivantaran48@gmail.com>
2024-06-11 16:31:46 +0000

    Fix user in admin

--- authorization/admin.py
+++ authorization/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 from authorization.models import User, ExecutorData
 
-admin.site.register(User)
+
 admin.site.register(ExecutorData)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'second_name', 'email', 'contact', 'is_staff', 'is_executor']
+    search_fields = ['first_name', 'second_name', 'email', 'contact', 'is_staff', 'is_executor']
